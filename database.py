0494986c09a55f6bc3cd3740b345b5b50c34d729
abdul-azeem-elsharkawy <abdulazeemlotfyelsharkawy@gmail.com>
2025-06-26 18:45:49 +0300

    add some explanation comments to the code

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
-import sqlite3
+import sqlite3     # importing sqlite3 module to work with SQLite database
 
-# إنشاء اتصال بقاعدة البيانات (أو إنشاؤها لو مش موجودة)
+# create a connection to the SQLite database
+# if the database file does not exist, it will be created
+# 'flights.db' is the name of the database file
 connnect = sqlite3.connect('flights.db')
 
-# إنشاء كائن cursor للتعامل مع قاعدة البيانات
+# create a cursor object using the connection
+# the cursor is used to execute SQL commands
 cursor = connnect.cursor()
 
-# إنشاء جدول الحجوزات لو مش موجود
+# Create a table for storing flight information
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS reservations (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,8 +22,10 @@ cursor.execute('''
     )
 ''')
 
-# حفظ التغييرات
+# save (commit) the changes to the database
+# this is important to ensure that the table is created
 connnect.commit()
 
-# إغلاق الاتصال
+# close the connection to the database
+# this is important to free up resources and avoid database locks
 connnect.close()
