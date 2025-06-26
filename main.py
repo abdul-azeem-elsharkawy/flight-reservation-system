6e7f86bcca1869aa08ec11ad877538d26eef0107
abdul-azeem-elsharkawy <abdulazeemlotfyelsharkawy@gmail.com>
2025-06-26 19:39:10 +0300

    minor improvements to the GUI

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
 import database  # importing the database.py module
+import os
 
 # create the main application window
 root = tk.Tk()
 root.title("Flight Reservation System")
 root.geometry("400x300")  # window size
+root.configure(bg="#f0f4f7") # set background color
 root.resizable(False, False)  # prevent resizing
+
+# add an icon to the window
+if os.path.exists("airplane-ticket (icon).ico"):
+    # If the icon file exists, set it as the window icon
+    root.iconbitmap("airplane-ticket (icon).ico")
 
 # This label will be used to display the title of the application
 title_label = ttk.Label(root, text="Welcome to Flight Reservation System", font=("Helvetica", 14, "bold"))
